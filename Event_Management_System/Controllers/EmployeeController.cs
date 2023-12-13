dfb1bc0e5ffb96e108bedc5aae1799ff723ceb89
Sumit Mohapatra <sumit.mohapatra@kare4u.in>
2023-12-13 17:25:59 +0530

    Entity folder created

---
+++ Event_Management_System/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Event_Management_System.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+    }
+}
---
+++ Event_Management_System/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Management_System.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailId { get; set; }
+    }
+}
